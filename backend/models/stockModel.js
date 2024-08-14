7b9857a65f8da7fb1f2713a9217f7308d4c57b35
Varun Rao <varunvellorerao@gmail.com>
2024-08-14 14:39:52 -0400

    Updating stock schema

---
+++ backend/models/stockModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const stockSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        symbol: {
+            type: String,
+            required: true,
+        },
+        industry: {
+            type: String,
+            required: true,
+        },
+        currPrice: {
+            type: Number,
+            required: true,
+        },
+        lastUpdated: {
+            type: Date,
+            required: true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+export const Stock = mongoose.model('Stock', stockSchema)
