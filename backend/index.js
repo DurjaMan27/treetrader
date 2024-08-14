1a213345b0229ce423b2e97e3f9860b0b88af150
Varun Rao <varunvellorerao@gmail.com>
2024-08-14 16:24:57 -0400

    applying cors policy

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,24 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Stock } from './models/stockModel.js';
 import stocksRoute from './routes/stocksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // middleware for parsing request body
 app.use(express.json());
+
+// middleware for handling CORS POLICY
+// Option 1: Allow ALL origins with default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom origins
+app.use(
+    cors({
+        origin: "https://localhost:3000",
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+)
 
 app.get('/', (request, response) => {
     console.log(request)
