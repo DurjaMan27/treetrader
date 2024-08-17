d0bc8fb83ed3d554ec5baf664cbc7da20b8ef8ac
Varun Rao <varunvellorerao@gmail.com>
2024-08-16 21:14:56 -0400

    Creating routes for login and register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import CreateStock from './pages/CreateStock';
 import ShowStock from './pages/ShowStock';
 import EditStock from './pages/EditStock';
 import DeleteStock from './pages/DeleteStock';
+import LoginScreen from './pages/LogIn';
+import RegisterScreen from './pages/LogIn';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/stocks/create' element={<CreateStock />} />
-      <Route path='/stocks/details/:id' element={<ShowStock />} />
-      <Route path='/stocks/edit/:id' element={<EditStock />} />
-      <Route path='/stocks/delete/:id' element={<DeleteStock />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<LoginScreen />} />
+        <Route path='/register' element={<RegisterScreen />} />
+        <Route path='/stocks/create' element={<CreateStock />} />
+        <Route path='/stocks/details/:id' element={<ShowStock />} />
+        <Route path='/stocks/edit/:id' element={<EditStock />} />
+        <Route path='/stocks/delete/:id' element={<DeleteStock />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
