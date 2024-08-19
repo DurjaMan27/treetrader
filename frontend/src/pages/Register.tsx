95550e26af893f6969e023fbeacfd6c159c72cd6
Varun Rao <varunvellorerao@gmail.com>
2024-08-19 13:22:45 -0400

    Updating register screen

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,98 @@
-
+import React, { useState, useEffect } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import ErrorMessage from '../components/ErrorMessage';
+import axios, { AxiosError } from 'axios';
 
 const RegisterScreen = () => {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmpassword) {
+      setMessage('Passwords do not match.');
+    } else {
+      setMessage('');
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+
+        setLoading(true);
+
+        const { data } = await axios.post(
+          "/users",
+          { username, email, password },
+          config
+        );
+
+        setLoading(false);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+      } catch (error: unknown) {
+        setError(error.response.data.message);
+      }
+    }
+  }
+
   return (
-    <div></div>
+    <div className='login-container'>
+      { error && <ErrorMessage variant='danger'>{ error }</ErrorMessage> }
+      { message && <ErrorMessage variant='danger'>{ message }</ErrorMessage> }
+      { loading && <Spinner /> }
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="name"
+            value={ username }
+            placeholder="Enter username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            value={ email }
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={ password }
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId='confirmPassword'>
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={ confirmpassword }
+            placeholder="Confirm password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+    </div>
   )
 }
 
