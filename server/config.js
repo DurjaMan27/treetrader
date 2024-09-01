db32ce8ed298cb4af5bf559e8bc1e21be17220e5
Varun Rao <varunvellorerao@gmail.com>
2024-09-01 18:08:59 -0400

    no more

--- server/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 
-    'mongodb+srv://varunvellorerao:zriKWt1SkX2QMks4@treetrader-mern.p3jth.mongodb.net/?retryWrites=true&w=majority&appName=TreeTrader-MERN'
