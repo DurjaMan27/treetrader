f5e4d00998833370fffc9f1ce7231c864520bfb7
Varun Rao <varunvellorerao@gmail.com>
2024-09-01 18:01:24 -0400

    updating folder names

---
+++ server/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://varunvellorerao:zriKWt1SkX2QMks4@treetrader-mern.p3jth.mongodb.net/?retryWrites=true&w=majority&appName=TreeTrader-MERN'
