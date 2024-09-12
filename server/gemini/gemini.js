9008ba417cd535f9b7adaee434bb2d9a4b5775f5
Varun Rao <varunvellorerao@gmail.com>
2024-09-12 14:57:42 -0400

    test

--- server/gemini/gemini.js
+++ server/gemini/gemini.js
@@ -26,7 +26,7 @@ const executePrompt = async (tickerInput) => {
     return response.text();
   } catch (error) {
     console.error('Error executing prompt:', error);
-    return 0;
+    return error;
   }
 }
 
